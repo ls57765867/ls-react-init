aa0391ae4de59ca576ca67f175ab723131a637bf
ls <ls57765867@126.com>
2023-12-26 21:54:07 +0800

    fix:添加PostCssPxToViewport排除node_modules 的功能

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,11 @@ export const PostCssPxToViewport = (options: Options = Options): Plugin => {
         postcssPlugin: 'postcss-px-to-viewport',
         Declaration(node) {
             // 有些px可能不需要转换  可以自定义名称
-            if (node.value.includes('px')) {
+            if (
+                !node.source.input.file.includes('/node_modules/') &&
+                parseFloat(node.value) > 12 &&
+                node.value.includes('px')
+            ) {
                 const num = parseFloat(node.value) // 考虑到有小数
                 node.value = `${((num / opt.viewportWidth) * 100).toFixed(2)}vw`
             }
